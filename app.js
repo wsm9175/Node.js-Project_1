ff2f98b0cd82a41d15c58d30cf5a664fd41bf685
wsm9175 <wsm9175@naver.com>
2021-05-26 14:28:41 +0900

    add index.js result.js, alter mainPage(plus button), app.js(DB connection), model/index.js

--- app.js
+++ app.js
@@ -1,41 +1,37 @@
 const express = require('express');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
-const session = require('express-session');
 const dotenv = require('dotenv');
 const path = require('path');
-const multer = require('multer');
 const fs = require('fs');
 const nunjucks = require('nunjucks');
 
 const { sequelize } = require('./models');
+const indexRouter = require('./routes/index');
+const resultRouter = require('./routes/result');
 
 dotenv.config() //dotenv에서 process.env로 만드는 과정 수행
 const app = express(); //server 실행. Express <== http를 다 포함함.
-app.use(session({secret: 'somevalue'}));
 app.set('port', process.env.PORT || 3000); //포트 설정
-app.set('views', path.join(__dirname, 'multipart.html'));
 app.set('view engine', 'html');
-nunjucks.configure('views',  {
+nunjucks.configure('views', {
       express: app,
-      watch: true,
+      while: true,
 });
 
+sequelize.sync({force: false})
+      .then(() => {
+            console.log('데이터베이스 연결 성공');
+      })
+      .catch((err) => {
+            console.error(err);
+      });
+      
 app.use(morgan('dev'));
 app.use('/', express.static(path.join(__dirname, 'public'))); //client에게 제공할 파일들을 pulic에 모아둠
 app.use(express.json()); //json처리하게 설정
 app.use(express.urlencoded({extended: false})); // 폼 처리
 app.use(cookieParser(process.env.COOKIE_SECRET));
-app.use(session({
-      resave:false,
-      saveUninitialized: false,
-      secret: process.env.COOKIE_SECRET,
-      cookie: {
-            httpOnly: true,
-            secure: false,
-      },
-      name: 'session-cookie'
-}));
 
 //upload파일 설정
 try{
@@ -45,33 +41,9 @@ try{
       fs.mkdirSync('uploads');
 }
 
-var user_file;
+app.use('/', indexRouter);
+// app.use('/result', resultRouter);
 
-const upload = multer({
-      storage: multer.diskStorage({ //올릴 부분에 대한 정보
-            destination(req, file, done){ // 실제로 파일이 올라갈 부분 명시
-                  done(null, 'uploads/');
-            },
-            filename(req, file, done){ //파일의 이름 명시를 통해 파일이 겹치는걸 방지
-                  const ext = path.extname(file.originalname);
-                  user_file = path.basename(file.originalname, ext) + Date.now() + ext; 
-                  done(null, user_file);
-            },
-      }),
-      limits: {filesize: 5 * 1024 * 1024}, //파일 사이즈
-});
-
-app.get('/', (req, res) => {
-      res.sendFile(path.join(__dirname, '/multipart.html'))
-});
-
-app.post('/', 
-      upload.fields([{name: 'txt'}]),
-      (req, res)=> {
-            console.log(req.files, req.body);
-            res.send('ok');
-      },
-);
 
 app.listen(app.get('port'), () => {
       console.log(app.get('port'), '3000번 포트에서 대기 중');
